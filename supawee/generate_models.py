f85d812c0bdf708e5a7b78bcc7f5c14a1b842335
Peter Csiba <petherz@gmail.com>
2024-08-23 10:04:57 -0700

    [bugfix] circular_deps_fields was wrongly indented, also make it a shell param

--- supawee/generate_models.py
+++ supawee/generate_models.py
@@ -9,7 +9,7 @@ import subprocess
 BASE_MODEL_NAME = "BaseDatabaseModel"
 
 
-def adjust_generated_models(model_file: str):
+def adjust_generated_models(model_file: str, circular_deps_fields: list[str]):
     with open(model_file, "r") as file:
         data = file.read()
 
@@ -52,8 +52,7 @@ def adjust_generated_models(model_file: str):
 
     data = data.replace("class BaseModel", "class " + BASE_MODEL_NAME)
 
-    # Hacks for circular deps
-    circular_deps_fields = ["merged_into"]
+    # Hacks for circular deps - tested in prod.
     for field_name in circular_deps_fields:
         pattern = re.compile(
             f"    {field_name}" + r" = ForeignKeyField\([\s\S]*?\)",
@@ -61,8 +60,10 @@ def adjust_generated_models(model_file: str):
         )
 
         # Text to replace with
-        replacement_text = f"""    # To overcome ForeignKeyField circular dependency
-        {field_name}_id = UUIDField(null=True)"""
+        replacement_text = (
+            "    # To overcome ForeignKeyField circular dependency\n"
+            f"    {field_name}_id = UUIDField(null=True)"
+        )
 
         # Replace
         data = re.sub(pattern, replacement_text, data)
@@ -81,11 +82,20 @@ def adjust_generated_models(model_file: str):
 def main():
     parser = argparse.ArgumentParser(description="Generate example models.")
     parser.add_argument("model_file", help="Path to the model file")
-    parser.add_argument("--host", default="localhost")
-    parser.add_argument("--port", default="54322")
-    parser.add_argument("--username", default="postgres")
-    parser.add_argument("--database", default="postgres")
-    parser.add_argument("--password", default="postgres")
+    parser.add_argument("--host", default="localhost", help="Host for the database")
+    parser.add_argument("--port", default="54322", help="Port for the database")
+    parser.add_argument(
+        "--username", default="postgres", help="Username for the database"
+    )
+    parser.add_argument("--database", default="postgres", help="Database name")
+    parser.add_argument(
+        "--password", default="postgres", help="Password for the database"
+    )
+    parser.add_argument(
+        "--circular_deps_fields",
+        default="",
+        help="Comma-separated field names for handling circular dependencies (without the _id suffix)",
+    )
     args = parser.parse_args()
 
     model_file = args.model_file
@@ -94,6 +104,7 @@ def main():
     username = args.username
     database = args.database
     password = args.password
+    circular_deps_fields = args.circular_deps_fields.split(",")
 
     env = os.environ.copy()
     # Unfortunately, passing password in the command line arguments somehow does NOT work
@@ -128,7 +139,7 @@ def main():
 
     # Format the model file
     subprocess.run(f"black {model_file}", shell=True)
-    adjust_generated_models(model_file)
+    adjust_generated_models(model_file, circular_deps_fields)
     subprocess.run(f"black {model_file}", shell=True)
 
 
